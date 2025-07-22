67e8e010887d67541ddb5c6936037e8d2d2add81
Sachethds <sachethds@gmail.com>
2025-07-22 12:32:32 +0530

    Add files via upload

---
+++ Assignment/Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by SACHETH D S. on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment/Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment
+//
+//  Created by SACHETH D S. on 22/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
